0f88f6c47de4fad2e38a4b1e3d5022c682379891
actionAxolot <mario.r.vallejo@gmail.com>
2012-08-22 11:44:53 -0500

    going to do some radical changes

--- pctv/apps/account/models.py
+++ pctv/apps/account/models.py
@@ -1,30 +1,47 @@
+# -*- coding: <utf-8> -*-
 from django.db import models
 from django.contrib.auth.models import User
 from apps.financial_channel.models import FinancialChannel
 from apps.prospection_channel.models import ProspectionChannel
 from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
 import datetime
 
 # Account module for PCTV software
 class Profile(models.Model):
-	user = models.OneToOneField(User)
-	salesman = models.ForeignKey(User, related_name='salesman')
-	visitation_date = models.DateTimeField(blank=False, null=False, default=datetime.datetime.today())
-	street = models.CharField(max_length=300, blank=False, null=False)
-	block = models.CharField(max_length=300, blank=False, null=False)
-	postal_code = models.IntegerField(blank=False, null=False)
-	municipality = models.CharField(max_length=300, blank=False, null=False)
-	state = models.CharField(max_length=100, blank=False, null=False)
-	phone = models.CharField(max_length=100, blank=False, null=False)
-	mobile = models.CharField(max_length=100, blank=False, null=False)
-	emergency_phone = models.CharField(max_length=100, blank=False, null=False)
-	total_income = models.DecimalField(max_digits=14, decimal_places=2, blank=False, null=False)
+	user = models.OneToOneField(User, verbose_name=_("user"))
+	salesman = models.ForeignKey(User, related_name='salesman', 
+		verbose_name_("salesperson"))
+	visitation_date = models.DateTimeField(blank=False, null=False, default=datetime.datetime.today(),
+		verbose_name=_("visitation date"))
+	street = models.CharField(max_length=300, blank=False, null=False, 
+		verbose_name=_("street"))
+	block = models.CharField(max_length=300, blank=False, null=False, 
+		verbose_name=_("block"))
+	postal_code = models.IntegerField(blank=False, null=False, 
+		verbose_name=_("postal code"))
+	municipality = models.CharField(max_length=300, blank=False, null=False, 
+		verbose_name=_("municipality"))
+	state = models.CharField(max_length=100, blank=False, null=False, 
+		verbose_name=_("state"))
+	phone = models.CharField(max_length=100, blank=False, null=False,
+		verbose_name=_("phone"))
+	mobile = models.CharField(max_length=100, blank=False, null=False,
+		verbose_name=_("mobile"))
+	emergency_phone = models.CharField(max_length=100, blank=False, null=False, 
+		verbose_name=_("emergency phone"))
+	total_income = models.DecimalField(max_digits=14, decimal_places=2, blank=False, null=False,
+		verbose_name=_("total income"))
 
-	financial_channel = models.ForeignKey(FinancialChannel)
-	prospection_channel = models.ForeignKey(ProspectionChannel)
+	financial_channel = models.ForeignKey(FinancialChannel, verbose_name=_("financial channel"))
+	prospection_channel = models.ForeignKey(ProspectionChannel, verbose_name=_("prospection channel"))
 
 	def __unicode__(self):
-		return u"%s's profile" % self.user
+		return _(u"%s's profile" % self.user)
+
+	class Meta:
+		verbose_name = _(u"Profile")
+		verbose_name_plural = _(u"Profiles")
 
 
 class ClientComments(models.Model):
