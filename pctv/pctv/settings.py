e7dac19a5f336de21ed864d1691d15cde8fb528f
actionAxolot <mario.r.vallejo@gmail.com>
2012-08-22 01:29:47 -0500

    Ready to deploy

--- pctv/pctv/settings.py
+++ pctv/pctv/settings.py
@@ -2,10 +2,18 @@
 import os
 
 # Set up variables used in the configuration file
-DEBUG = True
+
+if os.getenv("LOGNAME") == "axolote":
+    DEBUG = True
+    LOCAL_DEV = True
+else:
+    DEBUG = False
+    LOCAL_DEV = False
+
 TEMPLATE_DEBUG = DEBUG
 DIRNAME = os.path.abspath(__file__)
 PROJECT_DIRNAME = os.path.abspath(os.path.join(DIRNAME, '..', '..', '..'))
+
 
 # Profile configurations
 AUTH_PROFILE_MODULE = 'apps.account.Profile'
@@ -16,16 +24,28 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pctv',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+if LOCAL_DEV:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'pctv',                      # Or path to database file if using sqlite3.
+            'USER': 'root',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'actionaxolot_pc',                      # Or path to database file if using sqlite3.
+            'USER': 'actionaxolot_pc',                      # Not used with sqlite3.
+            'PASSWORD': 'mediaGartorifa21',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -35,7 +55,7 @@ TIME_ZONE = 'America/Mexico_City'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 SITE_ID = 1
 
@@ -50,20 +70,27 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_DIRNAME, 'assets', 'media')
+if LOCAL_DEV:
+    MEDIA_ROOT = os.path.join(PROJECT_DIRNAME, 'assets', 'media')
+else:
+    MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_DIRNAME, '..', 'staticapp', 'pctv', 'media'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIRNAME, 'static')
+if LOCAL_DEV:
+    STATIC_ROOT = os.path.join(PROJECT_DIRNAME, 'static')
+else:
+    STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_DIRNAME, '..', 'staticapp', 'pctv', 'static'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
