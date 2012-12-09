cbe11a933679321d51ee9adfc7244446d9377c37
actionAxolot <mario.r.vallejo@gmail.com>
2012-12-09 00:34:48 -0600

    Changed DB user and pass

--- pctv/pctv/settings.py
+++ pctv/pctv/settings.py
@@ -30,9 +30,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pctv',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'kiwisplice',                      # Or path to database file if using sqlite3.
+        'USER': 'kiwisplice',                      # Not used with sqlite3.
+        'PASSWORD': 'mediaGartorifa21',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
