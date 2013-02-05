e4a863909b4993edb7598a176b3fae70a0f0b7e4
Mario R. Vallejo <axolote@ambystoma.com>
2013-02-05 01:15:03 -0600

    Created a new mixing for CSV

--- pctv/apps/utils/views.py
+++ pctv/apps/utils/views.py
@@ -1,7 +1,12 @@
+"""
+This has so much potential. We could create a Parent mixin that could be inherited 
+that way we could render a lot of different formats.
+"""
 # Create your views here.
 from django.utils import simplejson as json
 from django.template import RequestContext, loader
 from django import http
+import csv
 
 
 class JSONTemplate(object):
@@ -34,3 +39,30 @@ class JSONRenderMixin(JSONTemplate):
     Just return plain old JSON to be consumed by the browser
     """
     pass
+
+
+class CSVRenderMixin(object):
+    """
+    Mixin for ListView. Override the render_to_response method 
+    so that it returns a nice csv file download thing
+    if certain get parameter is found
+    """
+    csv_filename = "really_awesome_csv.csv"
+
+    def render_csv_to_response(self, object_list):
+        """docstring for render_to_response"""
+        objects = object_list.values()
+        response = http.HttpResponse(mimetype="text/csv")
+        response["Content-Disposition"] = "attachment; filename=%s" % self.csv_filename
+        writer = csv.writer(response)
+        writer.writerow(self.get_row_titles(objects))
+        for o in objects:
+            row = list()
+            for k, v in objects.items():
+                row.append(v)
+            writer.writerow(row)
+        return response
+
+    def get_row_titles(self, objects):
+        """Get the row headers from the keys of an element in the object_list list"""
+        return objects[0].keys()
